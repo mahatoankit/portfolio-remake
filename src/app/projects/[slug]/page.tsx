7b121c0ed59dbab76c8a5979216ce78a5c81efd4
Ankit Mahato <chaudhryankit848@gmail.com>
2025-09-20 12:50:36 +0545

    small error fix

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>Project Title</h1>
+      <p>Project description goes here.</p>
+    </div>
+  )
+}
+
+export default page
